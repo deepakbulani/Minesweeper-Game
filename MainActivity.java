9877b501ed503b98584c6e91e242dda568434190
deepakbulani <bulani.deepak2@gmail.com>
2017-05-01 00:34:26 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.deepak.minesweeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GameEngine.getInstance().createGrid(this);
+    }
+}
